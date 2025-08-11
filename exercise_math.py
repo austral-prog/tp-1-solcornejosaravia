072610c1ae626923c1d946d7d9b57299f241b633
solcornejosaravia <scornejosaravia@mail.austral.edu.ar>
2025-08-11 12:50:22 -0300

    Update__exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,18 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    print(suma)
+    resta = a - b
+    print(resta)
+    mult = a * b
+    print(mult)
+    prom = (a + b)/2
+    print(prom)
+    cociente_int = a / b
+    print(int(cociente_int))
+    resto = a % b
+    print(resto)
+    cociente_fl = a / b
+    print(float(cociente_fl))
+math()
